b9c04f4d8264157cffc0895e1cd45269203d1ec4
Deron Decamp <decamp.deron@gmail.com>
2024-02-19 02:22:13 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home-page', component: HomePageComponent, title: 'Deron Decamp: ITS & SWE'},
+  {path: 'projects-page', component: ProjectsPageComponent, title: 'Deron Decamp: Projects'},
+  {path: '', component: HomePageComponent},
+  {path: '**', component: NotFoundPageComponent},
+];
