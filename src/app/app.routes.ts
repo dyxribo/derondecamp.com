51f0ded3be4f8f4fe97606ea06a6b9dfb4a1a33a
Deron Decamp <decamp.deron@gmail.com>
2024-02-20 02:51:42 -0500

    init contact page, rename route paths, fix cv button positioning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
-  {path: 'home-page', component: HomePageComponent, title: 'Deron Decamp: ITS & SWE'},
-  {path: 'projects-page', component: ProjectsPageComponent, title: 'Deron Decamp: Projects'},
+  {path: 'home', component: HomePageComponent, title: 'Deron Decamp: ITS & SWE'},
+  {path: 'projects', component: ProjectsPageComponent, title: 'Deron Decamp: Projects'},
+  {path: 'contact', component: ContactPageComponent, title: 'Deron Decamp: Contact'},
   {path: '', component: HomePageComponent},
   {path: '**', component: NotFoundPageComponent},
 ];
