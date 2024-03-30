a859ff6cc73ab702a2c20b849e5a591e6aa82efb
Deron Decamp <decamp.deron@gmail.com>
2024-03-29 21:52:46 -0400

    format files with prettier, remove hover on mobile, fix 404 page footer

--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   },
   standalone: true,
   imports: [],
-  
+
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.scss'
 })
